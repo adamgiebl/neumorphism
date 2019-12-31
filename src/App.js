7001e6fb207ad2ffd5450def243be0c75593236a
Adam <adamgiebl@gmail.com>
2019-12-31 19:01:09 +0100

    new stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import colorLuminance, { getContrast } from './lightendarken';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import React, { Component } from "react";
+import colorLuminance, { getContrast } from "./lightendarken";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import css from "react-syntax-highlighter/dist/esm/languages/prism/css";
+import { solarizedlight as atomLight } from "react-syntax-highlighter/dist/esm/styles/prism/";
+import * as s from "./App.style";
 
+SyntaxHighlighter.registerLanguage("css", css);
 
 class App extends Component {
   state = {
@@ -13,55 +15,62 @@ class App extends Component {
     shape: true,
     blur: 60,
     activeLightSource: 0
-  }
+  };
 
-  myRef = React.createRef();
-  body = document.getElementsByTagName('body')[0];
-  lightSources = []
+  softElement = React.createRef();
+  colorInput = React.createRef();
+  body = document.getElementsByTagName("body")[0];
+  lightSources = [];
 
-  lightColor = ""
-  darkColor = ""
+  lightColor = "";
+  darkColor = "";
 
   codeString = `background: linear-gradient(145deg);
 box-shadow: 30px 30px var(--blur) var(--lightColor), 
-            -30px -30px var(--blur) var(--darkColor);`
+            -30px -30px var(--blur) var(--darkColor);`;
 
-  handleOnChange = (e) => {
+  handleOnChange = e => {
     this.setState({
       [e.target.name]: e.target.value
-    })
+    });
+  };
+
+  validateColor = e => {
+    if (/^#[0-9A-F]{6}$/i.test(e.target.value)) {
+      this.handleOnChange(e);
+    }
   }
 
   toggleShape = () => {
     this.setState({
       shape: !this.state.shape
-    })
-  }
-
-  setLightSource = (e) => {
-    this.lightSources.forEach((e)=>{
-      e.classList.remove('active');
     });
-    e.target.classList.add('active');
-    this.setState({ activeLightSource: parseInt(e.target.dataset.value)});
-  }
+  };
+
+  setLightSource = e => {
+    this.lightSources.forEach(e => {
+      e.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    this.setState({ activeLightSource: parseInt(e.target.dataset.value) });
+  };
 
   componentDidMount() {
     this.setState({ color: "#968bf9" });
-    this.lightSources = [...document.getElementsByClassName('light-source')];
+    this.lightSources = [...document.getElementsByClassName("light-source")];
   }
 
   render() {
     const { blur, color, size, radius, shape, activeLightSource } = this.state;
-    if (this.myRef.current) {
+    if (this.softElement.current) {
       let angle = 145;
       let positionX = 30;
       let positionY = 30;
       const darkColor = colorLuminance(color, -0.15);
       const lightColor = colorLuminance(color, 0.15);
-      const firstGradientColor = colorLuminance(color, shape ? 0.07 : -0.10);
-      const secondGradientColor = colorLuminance(color, shape ? -0.10 : 0.07);
-      switch(activeLightSource) {
+      const firstGradientColor = colorLuminance(color, shape ? 0.07 : -0.1);
+      const secondGradientColor = colorLuminance(color, shape ? -0.1 : 0.07);
+      switch (activeLightSource) {
         case 1:
           positionX = 30;
           positionY = 30;
@@ -82,180 +91,159 @@ box-shadow: 30px 30px var(--blur) var(--lightColor),
           positionY = -30;
           angle = 45;
           break;
-        default: 
+        default:
           positionX = 30;
           positionY = 30;
           angle = 145;
           break;
       }
-      document.documentElement.style.setProperty('--positionX', `${positionX}px`);
-      document.documentElement.style.setProperty('--positionXOpposite', `${positionX * -1}px`);
-      document.documentElement.style.setProperty('--positionY', `${positionY}px`);
-      document.documentElement.style.setProperty('--positionYOpposite', `${positionY * -1}px`);
-      document.documentElement.style.setProperty('--angle', `${angle}deg`);
-      document.documentElement.style.setProperty('--blur', `${blur}px`);
-      document.documentElement.style.setProperty('--textColor', getContrast(color));
-      document.documentElement.style.setProperty('--baseColor', color);
-      document.documentElement.style.setProperty('--darkColor', darkColor);
-      document.documentElement.style.setProperty('--lightColor', lightColor);
-      document.documentElement.style.setProperty('--firstGradientColor', firstGradientColor);
-      document.documentElement.style.setProperty('--secondGradientColor', secondGradientColor);
-      this.myRef.current.style.setProperty('--size', size + 'px')
-      this.myRef.current.style.setProperty('--radius', radius + 'px')
+      this.colorInput.current.value = color;
+      document.documentElement.style.cssText = `
+        --positionX: ${positionX}px;
+        --positionXOpposite: ${positionX * -1}px;
+        --positionY: ${positionY}px;
+        --positionYOpposite: ${positionY * -1}px;
+        --angle: ${angle}deg;
+        --blur: ${blur}px;
+        --textColor: ${getContrast(color)};
+        --baseColor: ${color};
+        --darkColor: ${darkColor};
+        --lightColor: ${lightColor};
+        --firstGradientColor: ${firstGradientColor};
+        --secondGradientColor: ${secondGradientColor};
+      `;
+      this.softElement.current.style.setProperty("--size", size + "px");
+      this.softElement.current.style.setProperty("--radius", radius + "px");
 
-      this.codeString = `background: linear-gradient(${angle}deg, ${firstGradientColor}, ${secondGradientColor});
+      this.codeString = `border-radius: ${radius === "200" ? '50%' : radius + 'px'};
+background: linear-gradient(${angle}deg, ${firstGradientColor}, ${secondGradientColor});
 box-shadow: ${positionX}px ${positionY}px ${blur}px ${darkColor}, 
-            ${positionX * -1}px ${positionY * -1}px ${blur}px ${lightColor};`
+            ${positionX * -1}px ${positionY * -1}px ${blur}px ${lightColor};`;
     }
     return (
-      <Container className="App">
-        <h1><b>Soft</b> UI code</h1>
-        <Flex>
-          <Preview>
-            <Sun top="0" bottom="unset" right="0" left="unset" data-value="2" onClick={this.setLightSource} className="light-source"></Sun>
-            <Sun top="0" bottom="unset" right="unset" left="0" data-value="1" onClick={this.setLightSource} className="light-source active"></Sun>
-            <Sun top="unset" bottom="0" right="0" left="unset" data-value="3" onClick={this.setLightSource} className="light-source"></Sun>
-            <Sun top="unset" bottom="0" right="unset" left="0" data-value="4" onClick={this.setLightSource} className="light-source"></Sun>
-            <div
-              ref={this.myRef}
-              className="soft-element soft-shadow"
-            ></div>
-          </Preview>
-          <Configuration className="soft-shadow">
-            <input type="color" name="color" onChange={this.handleOnChange} placeholder="#ffffff"/>
-            <Slider 
+      <s.Container className="App">
+        <s.Flex>
+          <s.Preview>
+            <s.Sun
+              top="0"
+              bottom="unset"
+              right="0"
+              left="unset"
+              data-value="2"
+              onClick={this.setLightSource}
+              className="light-source"
+            ></s.Sun>
+            <s.Sun
+              top="0"
+              bottom="unset"
+              right="unset"
+              left="0"
+              data-value="1"
+              onClick={this.setLightSource}
+              className="light-source active"
+            ></s.Sun>
+            <s.Sun
+              top="unset"
+              bottom="0"
+              right="0"
+              left="unset"
+              data-value="3"
+              onClick={this.setLightSource}
+              className="light-source"
+            ></s.Sun>
+            <s.Sun
+              top="unset"
+              bottom="0"
+              right="unset"
+              left="0"
+              data-value="4"
+              onClick={this.setLightSource}
+              className="light-source"
+            ></s.Sun>
+            <div ref={this.softElement} className="soft-element soft-shadow"></div>
+          </s.Preview>
+          <s.Configuration className="soft-shadow">
+            <s.Row>
+              <span style={{ paddingRight: "10px" }}>Select color:</span>
+              <input
+                type="color"
+                name="color"
+                onChange={this.handleOnChange}
+                placeholder="#ffffff"
+                value={color}
+              />
+              <span style={{ margin: "0 10px" }}>or</span>
+              <input
+                type="text"
+                placeholder="#ffffff"
+                name="color"
+                ref={this.colorInput}
+                onChange={this.validateColor}
+              />
+            </s.Row>
+            <s.Row>
+              <label htmlFor="size" style={{ paddingRight: "10px" }}>Size: </label>
+              <s.Slider
+                type="range"
+                name="size"
+                value={size}
+                onChange={this.handleOnChange}
+                min="10"
+                max="350"
+                step="1"
+              />
+            </s.Row>
+            <s.Row>
+            <label htmlFor="radius" style={{ paddingRight: "10px" }}>Radius: </label>
+            <s.Slider
               type="range"
-              name="size" 
-              value={size}
-              onChange={this.handleOnChange}
-              min="10"
-              max="350"
-              step="1"
-            />
-            <Slider 
-              type="range"
-              name="radius" 
+              name="radius"
               value={radius}
               onChange={this.handleOnChange}
               min="0"
               max="200"
               step="1"
             />
-            <Slider 
+            </s.Row>
+            <s.Row>
+            <label htmlFor="blur" style={{ paddingRight: "10px" }}>Blur: </label>
+            <s.Slider
               type="range"
-              name="blur" 
+              name="blur"
               value={blur}
               onChange={this.handleOnChange}
               min="0"
               max="200"
               step="1"
             />
-            <ShapeSwitch>
-              <button className={shape ? "active" : ""} onClick={this.toggleShape}>
-                Concave
-              </button>
-              <button className={!shape ? "active" : ""} onClick={this.toggleShape}>
-                Convex
-              </button>
-            </ShapeSwitch>
-            <CodeBlock>
-              <SyntaxHighlighter language="css" style={atomDark}>
+            </s.Row>
+            <s.Row>
+              <label htmlFor="blur" style={{ paddingRight: "10px" }}>Shape: </label>
+              <s.ShapeSwitch>
+                <button
+                  className={shape ? "active" : ""}
+                  onClick={this.toggleShape}
+                >
+                  Concave
+                </button>
+                <button
+                  className={!shape ? "active" : ""}
+                  onClick={this.toggleShape}
+                >
+                  Convex
+                </button>
+              </s.ShapeSwitch>
+            </s.Row>
+            <s.CodeBlock>
+              <SyntaxHighlighter language="css" style={atomLight}>
                 {this.codeString}
               </SyntaxHighlighter>
-            </CodeBlock>
-          </Configuration>
-        </Flex>
-      </Container>
+            </s.CodeBlock>
+          </s.Configuration>
+        </s.Flex>
+      </s.Container>
     );
   }
 }
-
-const Container = styled.div`
-  max-width: 1000px;
-  height: 600px;
-  width: 100%;
-  text-align:center;
-`
-
-const Flex = styled.div`
-  display: flex;
-`
-
-const Preview = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 500px;
-  height: 500px;
-  margin-right: 50px;
-`
-
-const Sun = styled.div`
-  position: absolute;
-  left: ${props => props.left};
-  top: ${props => props.top};
-  right: ${props => props.right};
-  bottom: ${props => props.bottom};
-  background: #ffff00;
-  height: 30px;
-  width: 30px;
-  opacity: 0.3;
-  cursor: pointer;
-  border: 2px solid var(--textColor);
-  border-bottom-right-radius: ${props => props.right === "unset" && props.bottom === "unset" ? "30px" : "unset"};
-  border-bottom-left-radius: ${props => props.left === "unset" && props.bottom === "unset" ? "30px" : "unset"};
-  border-top-right-radius: ${props => props.right === "unset" && props.top === "unset" ? "30px" : "unset"};
-  border-top-left-radius: ${props => props.left === "unset" && props.top === "unset" ? "30px" : "unset"};
-
-  &.active {
-    opacity: 1;
-  }
-`
-
-const Slider = styled.input`
-
-`
-
-const CodeBlock = styled.div`
-  font-size: 10px;
-`
-
-const ShapeSwitch = styled.div`
-  display: flex;
-  overflow: hidden;
-  border-radius: 3px;
-  //border: 2px solid var(--textColor);
-
-
-  button {
-    flex-grow: 1;
-    padding: 10px 15px;
-    background: #30302F;
-    color: #F6F5F7;
-    cursor: pointer;
-    border: none;
-
-    &:first-child {
-      //border-right: 2px solid var(--textColor);
-    }
-
-    &.active {
-      background: #1D1D1B;
-    }
-  }
-`
-
-const Configuration = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 30px;
-  border-radius: 30px;
-  text-align: left;
-
-  > * {
-    margin-bottom: 20px;
-  }
-`
 
 export default App;
---
+++ src/App.style.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1000px;
+  width: 100%;
+  text-align:center;
+`
+
+export const Flex = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+export const Preview = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 500px;
+  margin-right: 50px;
+`
+
+export const Sun = styled.div`
+  position: absolute;
+  left: ${props => props.left};
+  top: ${props => props.top};
+  right: ${props => props.right};
+  bottom: ${props => props.bottom};
+  background: transparent;
+  height: 30px;
+  width: 30px;
+  cursor: pointer;
+  border: 2px solid var(--textColor);
+  border-bottom-right-radius: ${props => props.right === "unset" && props.bottom === "unset" ? "30px" : "unset"};
+  border-bottom-left-radius: ${props => props.left === "unset" && props.bottom === "unset" ? "30px" : "unset"};
+  border-top-right-radius: ${props => props.right === "unset" && props.top === "unset" ? "30px" : "unset"};
+  border-top-left-radius: ${props => props.left === "unset" && props.top === "unset" ? "30px" : "unset"};
+
+  &.active {
+    background: #ffff00;
+  }
+`
+
+export const Slider = styled.input`
+`
+
+export const CodeBlock = styled.div`
+  font-size: 10px;
+`
+
+export const ShapeSwitch = styled.div`
+  display: flex;
+  overflow: hidden;
+  border-radius: 3px;
+  flex-grow: 1;
+  //border: 2px solid var(--textColor);
+
+
+  button {
+    flex-grow: 1;
+    padding: 10px 15px;
+    background: #30302F;
+    color: #F6F5F7;
+    cursor: pointer;
+    outline: 0;
+    border: none;
+
+    &:first-child {
+      //border-right: 2px solid var(--textColor);
+    }
+
+    &.active {
+      background: #1D1D1B;
+    }
+  }
+`
+
+export const Configuration = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  border-radius: 30px;
+  text-align: left;
+`
+
+export const Row = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+  color: var(--textColor);
+  margin-bottom: 15px !Important;
+  margin-top: 0px !Important;
+`
