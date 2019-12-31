8f2e9cf1ec8671e3dab30cb9d89f7af1fb5e145d
Adam <adamgiebl@gmail.com>
2019-12-31 20:00:03 +0100

    Updates

---
+++ precache-manifest.d27d3351cbd6db1551cfb578df619970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da755c278d7780e07bd4670c8ef69586",
+    "url": "/SoftUI/index.html"
+  },
+  {
+    "revision": "777b329661569330305f",
+    "url": "/SoftUI/static/css/main.17662cd0.chunk.css"
+  },
+  {
+    "revision": "bd0d7d0c498e1a97ed9c",
+    "url": "/SoftUI/static/js/2.e2feb9f5.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/SoftUI/static/js/2.e2feb9f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "777b329661569330305f",
+    "url": "/SoftUI/static/js/main.1c27cf38.chunk.js"
+  },
+  {
+    "revision": "c2d481d6639d10773044",
+    "url": "/SoftUI/static/js/runtime-main.0eeba950.js"
+  }
+]);
