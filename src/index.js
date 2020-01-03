b1dd941ff3294bc7e61648370eebaf67dd269aab
Adam <adamgiebl@gmail.com>
2020-01-04 00:37:20 +0100

    perfect react-snap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import './index.scss';
 import './inputs.scss';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
